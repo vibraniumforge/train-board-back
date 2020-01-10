5162544ffc7fa498fe7d4d8d59bcdca0c8e4190b
vibraniumforge <gregm9999@gmail.com>
2020-01-10 15:47:25 -0800

    refactor validations

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,10 @@
 class Train < ApplicationRecord
 
-  validates_presence_of :destination, :origin, :remarks_boarding, :service, :trainno
+  validates :destination, presence: true
+  validates :origin, presence: true
+  validates :remarks_boarding, presence: true
+  validates :service, presence: true
+  validates :trainno, presence: true
 
   validates :scheduled, length: { is: 4 }
   validates :scheduled24, length: { is: 4 }
